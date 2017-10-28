05f3f87e6b79cf2c84b987df1a009357615ee4ee
Igor Kamyshev <garik.novel@gmail.com>
2017-10-28 21:25:29 +0700

    add map

---
+++ front/src/components/DataPoint.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+export default class DataPoint extends Component {
+    render() {
+        const {radius} = this.props
+
+        const greatPlaceStyle = {
+            position: 'absolute',
+            width: radius * 2,
+            height: radius * 2,
+            left: -radius,
+            top: -radius
+        };
+
+        return(
+            <div style={greatPlaceStyle}>
+                <svg>
+                    <circle cx={radius} cy={radius} r={radius} fill="red" />
+                </svg>
+            </div>
+        )
+    }
+}
--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Map = () => (
-    <div>
-        <p>Map!</p>
-    </div>
-);
+import axios from 'axios'
 
-export default Map
+import GoogleMap from 'google-map-react'
+import { css } from 'emotion'
+
+import DataPoint from './DataPoint'
+
+
+const mapContainer = css`
+    height: 800px;
+`;
+
+export default class Map extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            ranges: [(new Date()).getFullYear()],
+            currentYear: (new Date()).getFullYear(),
+            currentMetric: 'import',
+            dataItems: [],
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            ranges: [1999, 2000, 2001],
+            dataItems: [
+                {
+                    city: {
+                        lon: 84.948179,
+                        lat: 56.48466
+                    },
+                    radius: 10,
+                    amount: 1000000
+                },
+                {
+                    city: {
+                        lon: 86.084787,
+                        lat: 67.23976
+                    },
+                    radius: 10,
+                    amount: 1000000
+                }
+            ]
+        });
+    }
+
+    render() {
+        const dataItems = this.state.dataItems;
+
+        return (
+            <div className={mapContainer}>
+                <GoogleMap
+                    center={[67.239763, 86.084787]}
+                    zoom={3}
+                >
+                    {dataItems && dataItems.map((dataItem, index) =>
+                        <DataPoint
+                            key={index}
+                            lat={dataItem.city.lat}
+                            lng={dataItem.city.lon}
+                            radius={dataItem.radius}
+                            amount={dataItem.amount}
+                        />
+                    )}
+                </GoogleMap>
+            </div>
+        )
+
+    }
+}
