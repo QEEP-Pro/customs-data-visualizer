3ad7df890fa7138be35cbe4aae7418f01b4f2ea5
Igor Kamyshev <garik.novel@gmail.com>
2017-10-29 14:27:04 +0700

    [WIP] Pie Charts

--- front/src/components/detail/Detail.jsx
+++ front/src/components/detail/Detail.jsx
@@ -3,11 +3,15 @@ import React, {Component} from 'react'
 import { css } from 'emotion'
 
 import {Card, CardMedia, CardTitle} from 'material-ui/Card'
+import {Tabs, Tab} from 'material-ui/Tabs'
 import {List, ListItem} from 'material-ui/List'
 
-import {Line} from 'react-chartjs-2'
+import {Line, Pie} from 'react-chartjs-2'
 
-import {fetchDataItem} from '../../utils/api'
+
+import {fetchDataItem,fetchRange} from '../../utils/api'
+
+const _ = require('lodash');
 
 
 export default class Detail extends Component {
@@ -19,6 +23,7 @@ export default class Detail extends Component {
             fetched: false,
             activeCategory: null,
             data: [],
+            years: [],
         }
     }
 
@@ -27,6 +32,12 @@ export default class Detail extends Component {
             this.setState({
                 fetched: true,
                 data,
+            })
+        );
+
+        fetchRange().then(data =>
+            this.setState({
+                years: _.range(data.min, data.max),
             })
         );
     }
@@ -52,8 +63,6 @@ export default class Detail extends Component {
                 : <LineChart data={this.state.data.data[this.state.activeCategory.uid]} />
         ) : false;
 
-        console.log(this.state.fetched ? this.state.data.data : null)
-
         return (
             <div className={flexRow}>
                 <Card className={menuColumn}>
@@ -68,6 +77,38 @@ export default class Detail extends Component {
                         <CardMedia>
                             {lineChart}
                         </CardMedia>
+                    </Card>
+                    <br/>
+                    <Card>
+                        <CardTitle title={'Экспорт по странам'}/>
+                        <Tabs>
+                            {this.state.years.map( (year, index) =>
+                                <Tab label={year}>
+                                    {(!this.state.activeCategory && this.state.fetched) &&
+                                        <PieChart
+                                            data={this.state.data.totals[year].countries}
+                                            callback={getExportDataForPie}
+                                        />
+                                    }
+                                </Tab>
+                            )}
+                        </Tabs>
+                    </Card>
+                    <br />
+                    <Card>
+                        <CardTitle title={'Импорт по странам'}/>
+                        <Tabs>
+                            {this.state.years.map((year, index) =>
+                                <Tab label={year}>
+                                    {(!this.state.activeCategory && this.state.fetched) &&
+                                    <PieChart
+                                        data={this.state.data.totals[year].countries}
+                                        callback={getImportDataForPie}
+                                    />
+                                    }
+                                </Tab>
+                            )}
+                        </Tabs>
                     </Card>
                 </div>
             </div>
@@ -95,13 +136,13 @@ const LineChart = (props) => (
             datasets: [
                 {
                     label: 'Импорт',
-                    data: getImportData(props.data),
+                    data: getImportDataForLine(props.data),
                     borderColor: 'rgba(159, 168, 218, 1)',
                     backgroundColor: 'rgba(159, 168, 218, 0.3)',
                 },
                 {
                     label: 'Экспорт',
-                    data: getExportData(props.data),
+                    data: getExportDataForLine(props.data),
                     borderColor: 'rgba(255, 224, 130, 1)',
                     backgroundColor: 'rgba(255, 224, 130, 0.3)',
                 }
@@ -111,20 +152,56 @@ const LineChart = (props) => (
     />
 );
 
-const getImportData = (data) => {
+const PieChart = (props) => (
+    <Pie
+        data={{
+            datasets: [{
+                data: props.callback(props.data),
+                backgroundColor: [ "#4bb2c5 ", "#EAA228 ", "#c5b47f ", "#579575 ", "#839557 ", "#958c12 ", "#953579 ", "#4b5de4 ", "#d8b83f ", "#ff5800 ", "#0085cc ", "#c747a3 ", "#cddf54 ", "#FBD178 ", "#26B4E3 ", "#bd70c7 "]
+            }],
+            labels: getLabelsForPieChart(props.data),
+        }}
+    >
+    </Pie>
+);
+
+const getImportDataForLine = (data) => {
     return Object.keys(data).filter(item => !isNaN(parseInt(item)) ).map( (key, index) => ({
         x: key,
         y: data[key].import.amount,
     }));
 };
 
-const getExportData = (data) => {
+const getExportDataForLine = (data) => {
     return Object.keys(data).filter(item => !isNaN(parseInt(item)) ).map( (key, index) => ({
         x: key,
         y: data[key].export.amount,
     }));
 };
 
+const getExportDataForPie = (data) =>
+    Object.keys(data).map( (key, index) => {
+        return data[key].export.amount > 57667441 ? data[key].export.amount : null
+    });
+
+const getImportDataForPie = (data) =>
+    Object.keys(data).map( (key, index) => {
+        return data[key].export.amount > 57667441 ? data[key].import.amount : null
+    });
+
+const getLabelsForPieChart = (data) =>
+    Object.keys(data).filter(key => data[key].export.amount > 57667441 ? data[key].export.amount : null);
+
+
 const getLineChartOptions = () => ({
     layout: { padding: {left: 20, right: 20, top: 20, bottom: 20} },
+    scales: {
+        xAxes: [{
+            display: true,
+            scaleLabel: {
+                display: true,
+                labelString: 'Год'
+            }
+        }]
+    }
 });
