e9c87899140c07ef5e72e6f1da98bf38794e9e57
Igor Kamyshev <garik.novel@gmail.com>
2017-11-04 11:30:21 +0700

    adapt api client to new format

--- front/src/utils/api/detail.ts
+++ front/src/utils/api/detail.ts
@@ -8,59 +8,49 @@ import CountryDataItem from '../../model/CountryDataItem'
 
 
 export const fetchCategories = (regionId: number) =>
-    axios.get(`http://${HOSTNAME}:${PORT}/region?uid=${regionId}`)
-        .then(data => data.data.menu.map((item: any) => ({
+    axios.get(`http://${HOSTNAME}:${PORT}/regionCategories?uid=${regionId}`)
+        .then(data => data.data.map((item: any) => ({
             name: item.name,
             uid: parseInt(item.uid, 10)
         } as Category)))
 
 export const fetchTotals = (regionId: number) =>
-    axios.get(`http://${HOSTNAME}:${PORT}/region?uid=${regionId}`)
+    axios.get(`http://${HOSTNAME}:${PORT}/regionTotals?uid=${regionId}`)
         .then(data => {
-            const years = data.data.totals
+            const years = data.data
             return Object.keys(years).map((yearKey: string) => {
                 const countries = years[yearKey].countries
                 return {
                     year: parseInt(yearKey, 10),
-                    import: years[yearKey].import.amount,
-                    export: years[yearKey].export.amount,
+                    import: years[yearKey].import,
+                    export: years[yearKey].export,
                     countries: Object.keys(countries).map((countryKey: string) => ({
                         name: countryKey,
-                        import: countries[countryKey].import.amount,
-                        export: countries[countryKey].export.amount,
+                        import: countries[countryKey].import,
+                        export: countries[countryKey].export,
                     } as CountryDataItem))
                 } as DetailDataItem
             })
         })
 
 export const fetchIndustry = (regionId: number, industryId: number) =>
-        axios.get(`http://${HOSTNAME}:${PORT}/region?uid=${regionId}`)
+        axios.get(`http://${HOSTNAME}:${PORT}/regionData?uid=${regionId}&industryId=${industryId}`)
             .then(data => {
-                const industryKey = Object
-                    .keys(data.data.data)
-                    .map((key: string) => parseInt(key, 10))
-                    .filter((key: number) => !isNaN(key))
-                    .find((key: number) => key === parseInt(industryId.toString(), 10))
+                const years = data.data
 
-                if (industryKey) {
-                    const years = data.data.data[industryKey]
-
-                    return Object.keys(years)
-                        .filter((key: string) => !isNaN(parseInt(key, 10)))
-                        .map((yearKey: string) => {
-                            const countries = years[yearKey].countries
-                            return {
-                                year: parseInt(yearKey, 10),
-                                import: years[yearKey].import.amount,
-                                export: years[yearKey].export.amount,
-                                countries: Object.keys(countries).map((countryKey: string) => ({
-                                    name: countryKey,
-                                    import: countries[countryKey].import.amount,
-                                    export: countries[countryKey].export.amount,
-                                } as CountryDataItem))
-                            } as DetailDataItem
-                        })
-                } else {
-                    return []
-                }
+                return Object.keys(years)
+                    .filter((key: string) => !isNaN(parseInt(key, 10)))
+                    .map((yearKey: string) => {
+                        const countries = years[yearKey].countries
+                        return {
+                            year: parseInt(yearKey, 10),
+                            import: years[yearKey].import,
+                            export: years[yearKey].export,
+                            countries: Object.keys(countries).map((countryKey: string) => ({
+                                name: countryKey,
+                                import: countries[countryKey].import,
+                                export: countries[countryKey].export,
+                            } as CountryDataItem))
+                        } as DetailDataItem
+                    })
             })
